8f357f22ebb0d61aafcd373d93ff2da3afe0aa4e
laggykiller <chaudominic2@gmail.com>
2023-08-06 20:46:04 +0800

    Fix build

--- prepare-linux.sh
+++ prepare-linux.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-yum install -y boost-devel
+git clone https://github.com/boostorg/boost
+cd boost
+git submodule update --init
+mkdir build
+cd build
+cmake ..
+make
+make install
+cd ../../
 
 cd libpng
 mkdir build
--- prepare-mac.sh
+++ prepare-mac.sh
@@ -1,3 +1,29 @@
 #!/bin/sh
 
-brew install cmake boost libpng lzlib icu4c
+brew install cmake icu4c
+
+git clone https://github.com/boostorg/boost
+cd boost
+git submodule update --init
+mkdir build
+cd build
+cmake ..
+make
+make install
+cd ../../
+
+cd libpng
+mkdir build
+cd build
+cmake ..
+make
+make install
+cd ../../
+
+cd zlib
+mkdir build
+cd build
+cmake ..
+make
+make install
+cd ../../
