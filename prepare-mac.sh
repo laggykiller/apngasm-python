829017bb36142d93ec7af700b2d4f7a64c2a637c
laggykiller <chaudominic2@gmail.com>
2023-08-06 22:54:41 +0800

    Fix build

--- prepare-mac.sh
+++ prepare-mac.sh
@@ -1,26 +1,3 @@
 #!/bin/sh
 
-brew install cmake icu4c
-
-curl -O -L https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82_0.tar.gz
-tar -xf boost_1_82_0.tar.gz
-rm boost_1_82_0.tar.gz
-cd boost_1_82_0
-./bootstrap.sh
-./b2 install program_options regex system
-
-cd libpng
-mkdir build
-cd build
-cmake ..
-make -j
-make install
-cd ../../
-
-cd zlib
-mkdir build
-cd build
-cmake ..
-make -j
-make install
-cd ../../
+brew install cmake boost libpng lzlib icu4c
