d9a2757027d46fa77cef122be4cd6dd0c62ef3a9
laggykiller <chaudominic2@gmail.com>
2023-08-07 00:16:21 +0800

    Fix build

--- prepare-win.bat
+++ prepare-win.bat
@@ -7,25 +7,25 @@ mkdir usr
 cd zlib
 mkdir build
 cd build
-cmake "-DCMAKE_INSTALL_PREFIX:PATH=%cd%\..\..\usr" ..
+cmake ..
 cmake --build . --config Release -j
 cd ..\..\
 :: For some reason zconf.h disappear after compiling
 copy zlib\build\zconf.h zlib
-:: copy zlib\build\zconf.h usr\include
-:: copy zlib\zlib.h usr\include
-:: copy zlib\build\Release\zlib.lib usr\lib
+copy zlib\build\zconf.h usr\include
+copy zlib\zlib.h usr\include
+copy zlib\build\Release\zlib.lib usr\lib
 
 cd libpng
 mkdir build
 cd build
-cmake "-DZLIB_LIBRARY=%cd%\..\..\zlib\build\Release\zlib.lib" "-DZLIB_INCLUDE_DIR=%cd%\..\..\zlib" "-DCMAKE_INSTALL_PREFIX:PATH=%cd%\..\..\usr" ..
+cmake "-DZLIB_LIBRARY=%cd%\..\..\zlib\build\Release\zlib.lib" "-DZLIB_INCLUDE_DIR=%cd%\..\..\zlib" ..
 cmake --build . --config Release -j
 cd ..\..\
-:: copy libpng\png.h usr\include
-:: copy libpng\pngconf.h usr\include
-:: copy libpng\build\pnglibconf.h usr\include
-:: copy libpng\build\Release\libpng16.lib usr\lib
+copy libpng\png.h usr\include
+copy libpng\pngconf.h usr\include
+copy libpng\build\pnglibconf.h usr\include
+copy libpng\build\Release\libpng16.lib usr\lib
 
 :: mkdir boost
 :: cd boost
