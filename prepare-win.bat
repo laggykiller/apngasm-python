b9e45cfbfae924a6a94bb8f2f7687b493e37ff87
laggykiller <chaudominic2@gmail.com>
2023-08-08 04:04:24 +0800

    Fix build

--- prepare-win.bat
+++ prepare-win.bat
@@ -17,12 +17,6 @@ cmake "-DCMAKE_POLICY_DEFAULT_CMP0074=NEW" "-DCMAKE_INSTALL_PREFIX:PATH=%cd%/../
 cmake --build . --config Release --target INSTALL -j
 cd ..\..\
 
-:: C:
-:: cd C:\
-:: curl -O -L https://github.com/MarkusJx/prebuilt-boost/releases/download/1.81.0/boost-1.81.0-windows-2022-msvc-static-x86.tar.gz
-:: tar -xf boost-1.81.0-windows-2022-msvc-static-x86.tar.gz
-:: del boost-1.81.0-windows-2022-msvc-static-x86.tar.gz
-
 C:
 cd C:\
 curl -O -L https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82_0.zip
@@ -31,5 +25,5 @@ move boost_1_82_0 boost
 cd boost
 bootstrap.bat
 b2.exe
-copy stage\lib lib
-copy boost include
+xcopy stage\lib lib\ /E /H
+xcopy boost include\ /E /H
