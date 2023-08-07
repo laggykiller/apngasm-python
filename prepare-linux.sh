373c5bfce46f304c7413d7652b6458f6c1b70541
laggykiller <chaudominic2@gmail.com>
2023-08-07 22:11:53 +0800

    Fix build

--- prepare-linux.sh
+++ prepare-linux.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-yum update -y
-yum install -y boost-devel
-
 mkdir ./usr
+
+curl -O -L https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82_0.tar.gz
+tar -xf ./boost_1_82_0.tar.gz
+rm ./boost_1_82_0.tar.gz
+cd ./boost_1_82_0
+./bootstrap.sh
+./b2 install
+cd ../
 
 # zlib and libpng usually already exist in Linux system,
 # need to isolate own and system build!
