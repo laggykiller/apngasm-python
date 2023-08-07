b0bea7ab90156d01214e2c50c8ee99906d812d78
laggykiller <chaudominic2@gmail.com>
2023-08-08 02:21:37 +0800

    Fix build

--- prepare-mac.sh
+++ prepare-mac.sh
@@ -5,13 +5,13 @@ brew install cmake boost icu4c
 cd ./zlib
 mkdir build
 cd ./build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=../../usr ..
+cmake -DCMAKE_INSTALL_PREFIX:PATH=$(pwd)/../../usr ..
 make install -j
 cd ../../
 
 cd ./libpng
 mkdir build
 cd ./build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=../../usr ..
+cmake -DCMAKE_INSTALL_PREFIX:PATH=$(pwd)/../../usr -DZLIB_ROOT=../../usr ..
 make install -j
 cd ../../
