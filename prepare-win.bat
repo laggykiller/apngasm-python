91f008ba6bb2ca21b0a0948079d1ac18e5d059fb
laggykiller <chaudominic2@gmail.com>
2023-08-09 00:45:53 +0800

    Fix build

--- prepare-win.bat
+++ prepare-win.bat
@@ -17,14 +17,14 @@ cd build
 cmake -DCMAKE_C_FLAGS_RELEASE="/MT" -DBUILD_SHARED_LIBS=OFF -DCMAKE_POLICY_DEFAULT_CMP0074=NEW -DCMAKE_INSTALL_PREFIX:PATH=C:\opt\libpng -DZLIB_ROOT=C:\opt\zlib -DZLIB_USE_STATIC_LIBS=ON -DPNG_SHARED=OFF ..
 cmake --build . --target INSTALL --config Release
 
-cd C:\opt
+chdir C:\opt
 curl -O -L "https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82_0.zip"
 tar -xf boost_1_82_0.zip
 del boost_1_82_0.zip
 move boost_1_82_0 boost
 cd boost
-call bootstrap.bat --prefix=.
-b2.exe install --build-dir=tmp --prefix=. --build-type=complete --with-program_options --with-regex --with-system -j4 msvc stage
+call bootstrap.bat
+b2.exe install --build-dir=tmp --build-type=complete --with-program_options --with-regex --with-system -j4 msvc stage
 robocopy include\boost-1_82\boost include\boost /E
 
 exit 0
