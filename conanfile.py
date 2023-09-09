0478ea2ab857d56a1099937219ca8785dcaa637d
laggykiller <chaudominic2@gmail.com>
2023-09-09 22:24:01 +0800

    Condition cmake dependency

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conan import ConanFile
+import shutil
 
 class ApngasmRecipe(ConanFile):
     settings = 'os', 'compiler', 'build_type', 'arch'
@@ -6,12 +7,13 @@ class ApngasmRecipe(ConanFile):
     def requirements(self):
         self.requires("zlib/1.2.13")
         self.requires("libpng/1.6.40")
-        self.requires("boost/1.73.0")
+        self.requires("boost/1.69.0")
         self.generators = ['CMakeToolchain', 'CMakeDeps']
 
     def build_requirements(self):
-        # https://stackoverflow.com/questions/42123509/cmake-finds-boost-but-the-imported-targets-not-available-for-boost-version
-        self.tool_requires("cmake/[>=3.27]")
+        self.build_requires("b2/4.5.0")
+        if not shutil.which('cmake'):
+            self.tool_requires("cmake/[>=3.27]")
     
     def build(self):
         build_type = 'Release'
