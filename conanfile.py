c3a592f2fadad40a66ab7bf8ef1a822f30774e76
laggykiller <chaudominic2@gmail.com>
2023-09-10 05:45:23 +0800

    Fix dependency detection

--- conanfile.py
+++ conanfile.py
@@ -38,31 +38,8 @@ class ApngasmRecipe(ConanFile):
         'foreach.hpp'
     ]
 
-    def check_lib_present(self, target):
-        if platform.system() != 'Linux':
-            return False
-        
-        result = subprocess.run(
-            ['sh', '-c', "ldconfig -p | tail -n +2 | grep -o '/.*/' | sort -u"],
-            stdout = subprocess.PIPE,
-            stderr = subprocess.PIPE
-            )
-        lib_dirs = result.stdout.decode().split('\n')
-
-        for lib_dir in lib_dirs:
-            if not os.path.exists(lib_dir):
-                continue
-                
-            if target in os.listdir(lib_dir):
-                return True
-        
-        return False
-
-    def check_header_present(self, target):
-        if platform.system() != 'Linux':
-            return False
-        
-        if target in os.listdir('/usr/include'):
+    def check_file_present(self, target, search_path):        
+        if target in os.listdir(search_path):
             return True
         
         return False
@@ -71,20 +48,20 @@ class ApngasmRecipe(ConanFile):
         self.generators = ['CMakeToolchain', 'CMakeDeps']
 
         if not (platform.system() == 'Linux' and 
-                all([self.check_lib_present(i) for i in self.zlib_libs]) and
-                all([self.check_header_present(i) for i in self.zlib_headers])):
+                all([self.check_file_present(i, '/usr/lib') for i in self.zlib_libs]) and
+                all([self.check_file_present(i, '/usr/include') for i in self.zlib_headers])):
             
             self.requires("zlib/1.2.13")
         
         if not (platform.system() == 'Linux' and 
-                all([self.check_lib_present(i) for i in self.libpng_libs]) and
-                all([self.check_header_present(i) for i in self.libpng_headers])):
+                all([self.check_file_present(i, '/usr/lib') for i in self.libpng_libs]) and
+                all([self.check_file_present(i, '/usr/include') for i in self.libpng_headers])):
             
             self.requires("libpng/1.6.40")
 
         if not (platform.system() == 'Linux' and
-            all([self.check_lib_present(i) for i in self.boost_libs]) and
-            all([self.check_header_present(i) for i in self.boost_headers])):
+            all([self.check_file_present(i, '/usr/lib') for i in self.boost_libs]) and
+            all([self.check_file_present(i, '/usr/include/boost') for i in self.boost_headers])):
 
             self.requires("boost/1.82.0")
 
