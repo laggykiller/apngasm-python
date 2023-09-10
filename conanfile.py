1fa09441b3f9d77e3ed07b746a157fba8634e965
laggykiller <chaudominic2@gmail.com>
2023-09-10 13:23:54 +0800

    Try older boost/b2

--- conanfile.py
+++ conanfile.py
@@ -1,25 +1,18 @@
 from conan import ConanFile
-import os
 import shutil
 
 class ApngasmRecipe(ConanFile):
     settings = 'os', 'compiler', 'build_type', 'arch'
 
-    def check_file_present(self, target, search_path):        
-        if target in os.listdir(search_path):
-            return True
-        
-        return False
-    
     def requirements(self):
         self.requires("zlib/1.2.13")
         self.requires("libpng/1.6.40")
-        self.requires("boost/1.82.0")
+        self.requires("boost/1.74.0")
 
         self.generators = ['CMakeToolchain', 'CMakeDeps']
 
     def build_requirements(self):
-        self.build_requires("b2/[>4.10]")
+        self.build_requires("b2/4.5.0")
         if not shutil.which('cmake'):
             self.tool_requires("cmake/[>=3.27]")
     
