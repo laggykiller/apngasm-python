97bff660887a856562e211350fe80c17a6948683
laggykiller <chaudominic2@gmail.com>
2023-09-10 12:47:58 +0800

    Fix build

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 import shutil
 import get_arch
-from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 from conan.tools.apple import is_apple_os
 
 class ApngasmRecipe(ConanFile):
@@ -10,11 +10,10 @@ class ApngasmRecipe(ConanFile):
     def requirements(self):
         self.requires("zlib/1.2.13")
         self.requires("libpng/1.6.40")
-        self.requires("boost/1.82.0")
-        self.generators = ['CMakeToolchain', 'CMakeDeps']
+        self.requires("boost/1.74.0")
 
     def build_requirements(self):
-        self.build_requires("b2/[>4.10]")
+        self.build_requires("b2/4.5.0")
         if not shutil.which('cmake'):
             self.tool_requires("cmake/[>=3.27]")
     
@@ -23,6 +22,8 @@ class ApngasmRecipe(ConanFile):
     
     def generate(self):
         tc = CMakeToolchain(self)
+        cmake = CMakeDeps(self)
         if is_apple_os(self) and get_arch == 'universal2':
             tc.blocks['apple_system'].values['cmake_osx_architectures'] = 'x86_64; arm64'
         tc.generate()
+        cmake.generate()
