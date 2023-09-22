e4bc7b8111a3fb02c3e0cdf3cf8c291e19c22ad6
laggykiller <chaudominic2@gmail.com>
2023-09-23 04:21:17 +0800

    Using official sphinx-autoapi

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,6 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
-import sys
 
 project = 'apngasm-python'
 copyright = '2023, laggykiller'
