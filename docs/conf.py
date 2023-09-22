0b70efa2a2ac936b22d1236e49c391b3719c86bc
laggykiller <chaudominic2@gmail.com>
2023-09-23 03:56:02 +0800

    Use own fork of sphinx-autoapi

--- docs/conf.py
+++ docs/conf.py
@@ -33,10 +33,8 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 autoapi_dirs = [
-    os.path.abspath('../src-python/apngasm_python'),
-    os.path.abspath('../src-python/apngasm_python/_apngasm_python')
+    os.path.abspath('../src-python')
     ]
-autoapi_python_use_implicit_namespaces = True
 autoapi_file_patterns = ["*.pyi", "*.py"]
 
 # -- Options for HTML output -------------------------------------------------
