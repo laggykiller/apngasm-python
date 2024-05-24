465328b03dcff064651c35f04b816ddd49beee71
laggykiller <chaudominic2@gmail.com>
2024-05-24 09:45:52 +0800

    Set CMAKE_GENERATOR manually

--- PreLoad.cmake
+++ PreLoad.cmake
@@ -2,13 +2,11 @@
 # Need python headers and libraries, but msvc not able to find them
 # If inside cygwin or msys.
 
-if (WIN32 AND NOT MSVC)
-    execute_process(COMMAND uname OUTPUT_VARIABLE uname)
-    if (uname MATCHES "^MINGW")
-        set (CMAKE_GENERATOR "MinGW Makefiles" CACHE INTERNAL "" FORCE)
-    elseif (uname MATCHES "^MSYS")
-        set (CMAKE_GENERATOR "MSYS Makefiles" CACHE INTERNAL "" FORCE)
-    else ()
-        set (CMAKE_GENERATOR "Ninja" CACHE INTERNAL "" FORCE)
-    endif()
-endif()
+# Use this for MinGW
+# set (CMAKE_GENERATOR "MinGW Makefiles" CACHE INTERNAL "" FORCE)
+
+# Use this for MSYS
+# set (CMAKE_GENERATOR "MSYS Makefiles" CACHE INTERNAL "" FORCE)
+
+# Use this otherwise
+set (CMAKE_GENERATOR "Ninja" CACHE INTERNAL "" FORCE)
